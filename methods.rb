69f25a6881ee4f29f7fa344a7788e50d52a44954
Apoh <Edenra08@gmail.com>
2022-07-23 20:11:00 +0100

    Assignment completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,51 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+
+
+def greet name 
+    puts "Hello, #{name}!"
+
+end
+    greet "Mary"
+
+
+  def greet_with_default(name = "programmer" )
+        puts "Hello, #{name}!"
+
+ end
+
+ greet_with_default
+
+ greet_with_default "Sunny"
+
+
+
+
+ def add (num1, num2)
+   return num1 + num2
+end
+
+sum = add(1, 2);
+
+  
+
+
+
+def halve(num)
+    if num.class === Integer
+        num/2
+    
+end
+
+return nil
+end
+
+
+result = halve(4);
+
